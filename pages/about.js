3bfbc27210c6b7ccf70a9ac1649624f962006306
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2022-11-17 12:38:34 +0200

    Next js

---
+++ pages/about.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+    
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo
+        magni numquam molestiae veniam itaque quisquam expedita commodi sit,
+        quidem harum necessitatibus porro et sapiente eligendi velit ad nam
+        impedit! Ipsa!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, est
+        aliquam in necessitatibus porro laboriosam ab error earum eius aliquid
+        tempora id saepe eligendi natus autem soluta debitis. Officia,
+        excepturi.
+      </p>
+    </div></>
+  );
+};
+
+export default About;
